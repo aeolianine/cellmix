dcedeb5ab33c1aa038d97b49c402a47a2d5c09a6
David Ardell <dardell@ucmerced.edu>
2017-12-28 12:59:45 -0800

    Update utils.R

--- pkg/CellMix/R/utils.R
+++ pkg/CellMix/R/utils.R
@@ -431,6 +431,7 @@ uq_requirePackage <- function(package, lib=NULL, ..., load=TRUE, msg=NULL, quiet
 	## install
 	# check Bioconductor repositories
 	hasRepo <- function(p){ any(grepl(p, getOption('repos'))) } 
+	install_type <- NULL
 	if( ptype == 'CRAN-like' 
 		|| ( ptype == 'BioC' && hasRepo('/((bioc)|(data/annotation))/?$') )
 		|| ( ptype == 'BioCSoft' && hasRepo('/bioc/?$') )
